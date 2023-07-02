f903a847007e171bfeeb0c6af14ed3cdf3102d2a
irfan1718 <its2irfan17@gmail.com>
2023-07-03 01:16:28 +0530

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button';
+
+function Home() {
+  return (
+    <div className='p-5'>
+      <Button size='sm' variant='default'>
+        Admin Dashboard
+      </Button>
+    </div>
+  );
+}
+
+export default Home;
