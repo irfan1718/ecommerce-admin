4b945c534911fc58c0f0255ae87bc77680b71121
irfan1718 <its2irfan17@gmail.com>
2023-07-05 02:51:16 +0530

    zustand, store modal, provider

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,22 @@
-import { UserButton } from '@clerk/nextjs';
+/* eslint-disable react-hooks/rules-of-hooks */
+'use client';
+import { useEffect } from 'react';
+
+import { useStoreModal } from '@/hooks/use-store-modal';
 
 const setUpPage = () => {
-  return (
-    <div>
-      <div className='p-4'>
-        <UserButton afterSignOutUrl='/' />
-      </div>
-      <div className='flex justify-center items-center h-full'>
-        This is a protected route!
-      </div>
-    </div>
-  );
+
+const onOpen = useStoreModal((state) => state.onOpen);
+const isOpen = useStoreModal((state) => state.isOpen);
+
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [onOpen, isOpen]);
+
+  return <div className='p-4'>Protected Route</div>;
 };
 
 export default setUpPage;
